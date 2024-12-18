843a67298aa84183a2192f8877b67bb4bb69100f
davidstana99 <dstana@ford.com.tr>
2024-12-18 13:00:51 +0200

    ...

--- DiaryApp/obj/Debug/net8.0/DiaryApp.AssemblyInfo.cs
+++ DiaryApp/obj/Debug/net8.0/DiaryApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiaryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6ee218145263f8231d6034d684a180782e022d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiaryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiaryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
